badf3382527aa962ebe7f1866ffd7efffba7e5ee
jasition <jasition@gmail.com>
2024-11-30 13:33:25 +0000

    Split main functions

--- chapter-9/app/src/test/kotlin/Cqrs-usage.kt
+++ chapter-9/app/src/test/kotlin/Cqrs-usage.kt
@@ -1,53 +1,7 @@
-import java.time.Instant
 import java.util.UUID
 
 fun main() {
-    /*eventSourceUsage()*/
     cqrsUsage()
-}
-
-fun eventSourceUsage() {
-    val contractId = UUID.randomUUID()
-    val eventStore = EventStore<UUID, ContractEvent>()
-
-    val createdEvent = ContractDraftedEvent(
-        contractId = contractId,
-        time = Instant.now(),
-        draftedByHousehold = "HouseholdA",
-        counterpartyHousehold = "HouseholdB",
-        serviceProvided = "Cleaning",
-        serviceReceived = "Babysitting"
-    )
-    val amendedEvent = ContractAmendedEvent(
-        contractId = contractId,
-        targetVersion = 1,
-        time = Instant.now(),
-        amendedByHousehold = "HouseholdB",
-        serviceReceivedUpdate = "Dish washing",
-        serviceProvidedUpdate = null
-    )
-    val agreedEventByHouseholdA = ContractAgreedEvent(
-        contractId = contractId,
-        targetVersion = 2,
-        time = Instant.now(),
-        agreedByHousehold = "HouseholdA"
-    )
-    val agreedEventByHouseholdB = ContractAgreedEvent(
-        contractId = contractId,
-        targetVersion = 3,
-        time = Instant.now(),
-        agreedByHousehold = "HouseholdB"
-    )
-
-    listOf(
-        createdEvent,
-        amendedEvent,
-        agreedEventByHouseholdA,
-        agreedEventByHouseholdB
-    ).forEach { eventStore.append(contractId, it) }
-
-    val aggregate = eventStore.get(contractId)?.play()
-    println("Aggregate is of version: ${aggregate?.version}")
 }
 
 fun cqrsUsage() {
